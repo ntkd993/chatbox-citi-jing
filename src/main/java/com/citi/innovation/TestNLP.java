95c8e29dd96808123d92b132af857a208869969a
zj <zj@windows10.microdone.cn>
2020-09-12 12:32:07 +0800

    initial push

---
+++ src/main/java/com/citi/innovation/TestNLP.java
@@ -0,0 +1,7 @@
+package com.citi.innovation;
+
+public class TestNLP {
+	public static void main(String[]args) {
+		
+	}
+}
---
+++ src/test/java/com/citi/innovation/NLPTest.java
@@ -0,0 +1,5 @@
+package com.citi.innovation;
+
+public class NLPTest {
+
+}
